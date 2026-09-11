2749d3da25e46daee54040d6ce5617f3efc35b80
Vardet <lundgrennisse@gmail.com>
2026-09-11 16:26:43 +0200

    Linnea legal stub

---
+++ js/legal.js
@@ -0,0 +1,6 @@
+export const legalDisclaimer = "Intressebevakning, inte juridisk rådgivning.";
+export const projectLegal = [];
+export const standingRules = [];
+export function legalBlock() { return legalDisclaimer; }
+export function legalBrief() { return legalDisclaimer; }
+export function legalRevisionSummary() { return legalDisclaimer; }
