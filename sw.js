8d1fd3e7eba5cb388fd84ee0701ed5d56a8a685a
Vardet <lundgrennisse@gmail.com>
2026-09-11 16:51:05 +0200

    Linnea PWA v15: full runtime, Swedish voice after tap, SVG avatar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "linnea-shell-v14";
+const CACHE = "linnea-shell-v15";
 const SHELL = [
   "./",
   "./index.html",
@@ -13,14 +13,17 @@ const SHELL = [
   "./js/composer.js",
   "./icons/linnea.svg"
 ];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
 });
+
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
   );
 });
+
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
   if (url.pathname.includes("/api/")) return;
